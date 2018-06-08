3a0f6b7b38ccefb626b05becc716c54913831cf0
marcio <mpschiehl@outlook.com>
2018-06-08 08:12:01 -0300

    Inicialização do Repositorio

---
+++ ListaArraylist/src/listaarraylist/Lista01.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listaarraylist;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Lista01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        numeros.add(11);
+        numeros.add(32);
+        numeros.add(98);
+        numeros.add(25);
+        numeros.add(19);
+        numeros.add(18);
+        numeros.add(15);
+        numeros.add(31);
+        numeros.add(12);
+        numeros.add(13);
+        numeros.add(14);
+        numeros.add(19);
+        numeros.add(26);
+        numeros.add(41);
+        numeros.add(42);
+        System.out.println(numeros.get(0) + "\n" + numeros.get(1) + "\n"
+                + numeros.get(2) + "\n" + numeros.get(3) + "\n" + numeros.get(4) + "\n"
+                + numeros.get(5) + "\n" + numeros.get(6) + "\n" + numeros.get(7) + "\n"
+                + numeros.get(8) + "\n" + numeros.get(9) + "\n" + numeros.get(10) + "\n"
+                + numeros.get(11) + "\n" + numeros.get(12) + "\n" + numeros.get(13) + "\n"
+                + numeros.get(14)
+        );
+
+    }
+
+}
